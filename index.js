7ea3f201543ab239efbbbbf4fb2990e1981f2c3e
Tejash Shrestha <hiphop.shrestha32@gmail.com>
2019-02-15 22:33:00 +0530

    Stored mail threads in a list with no duplicates

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const path = require('path');
 var request = require('request-promise');
 var __dirname = "G:/git-projects/gmailXerox";
-
+var threads =[];
 app.use(Session({
     secret: 'tejash-secret-19890913007',
     resave: true,
@@ -53,10 +53,10 @@ app.get("/tokens", function(req, res) {
 });
 
 function listmessage(access_token){
-	console.log(access_token);
+	console.log(access_token);	
 request({
   "method":"GET", 
-  "uri": "https://www.googleapis.com/gmail/v1/users/me/messages?includeSpamTrash=false&maxResults=10",
+  "uri": "https://www.googleapis.com/gmail/v1/users/me/messages?q=newer_than:1d",
   "json": true,
   "headers": {
     "User-Agent": "google-api-nodejs-client/0.7.2 (gzip)",
@@ -67,12 +67,13 @@ request({
   	console.log(res.messages);
   	console.log(res.messages[0]);*/
   	for(var i = 0; i < res.messages.length;i++){
-        getmessage(res.messages[i], access_token);
-  }
+        getmessage(res.messages[i], access_token,res.messages.length);
+  	}
+
   });
 }
 
-function getmessage(message,access_token){
+function getmessage(message,access_token,max_length){
 	console.log(message);
 	request({
   	"method":"GET", 
@@ -82,11 +83,57 @@ function getmessage(message,access_token){
 	    "User-Agent": "google-api-nodejs-client/0.7.2 (gzip)",
 	    "Authorization": "Bearer "+ access_token,    
 	    "Accept": 'application/json'}
-  }).then(function(res){
+  }).then(function(res){  	
+  	var thread={};
+  	thread["id"]=res.threadId;
+  	thread["message"]=res.snippet;
+  	for(var i =0; i<res.payload.headers.length;i++){
+
+  		if(res.payload.headers[i].name=="Subject"){
+  			//console.log(res.payload.headers[i].value);
+  			thread["subject"]=res.payload.headers[i].value;
+  		}
+  		if(res.payload.headers[i].name=="Date"){
+  			//console.log(res.payload.headers[i].value);
+  			thread["date"]=res.payload.headers[i].value;
+  		}
+  		if(res.payload.headers[i].name=="From"){
+  			//console.log(res.payload.headers[i].value);
+  			thread["from"]=res.payload.headers[i].value;
+  		}
+
+  	}
+  	threads.push(thread);
+  	if(threads.length==max_length){
+  		console.log(threads.length);
+  		for(var i =0; i<threads.length;i++){
+    		msg =[threads[i].message];
+    		for(var j=i+1; j<threads.length;j++){
+        		if(threads[i].id==threads[j].id){
+            		msg.push(threads[j].message);
+            		threads.splice(j, 1);
+            		if(j==i+1){
+            			j--;
+            		}
+        		}
+    		}
+			if(msg.length>1){
+	        	threads[i].message= msg;
+	    	}	
+
+		}
+
+		console.log(threads.length);
+		console.log(threads);
+ 	}
+  
+  	
+  	/*var subject= JSON.parse(res.payload.headers);
+  	console.log(subject[''])
   	console.log(res.payload.headers[0]);
   	console.log(res.payload.headers[14]);
   	console.log(res.payload.headers[16]);
-  	console.log(res.snippet);
+  	console.log(res.snippet);*/
   	
 });
 }
